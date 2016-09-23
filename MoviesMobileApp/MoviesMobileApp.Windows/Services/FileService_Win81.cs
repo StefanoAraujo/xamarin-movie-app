8fb683c16e821697ba0b314147c3d332aa056622
martin tesar <tesar.cze@gmail.com>
2016-09-23 09:52:33 +0200

    Add ISQLite interface and add FileService_Win 8.1

---
+++ MoviesMobileApp/MoviesMobileApp.Windows/Services/FileService_Win81.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoviesMobileApp.Services;
+using MoviesMobileApp.Windows.Services;
+using Windows.Storage;
+
+[assembly: Xamarin.Forms.Dependency(typeof(FileService_Win81))]
+namespace MoviesMobileApp.Windows.Services
+{
+    class FileService_Win81 :IFileService
+    {
+        public string FileStoragePath
+        {
+            get
+            {
+                return ApplicationData.Current.LocalFolder.Path;
+            }
+        }
+    }
+}
